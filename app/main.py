ea639ae9124a780fac50e49299b24638fc959ada
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2025-02-08 17:35:32 +0200

    добавил админзону

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from sqladmin import Admin, ModelView
 
 from app.core.config import settings
+from app.core.db import engine
+from app.models import User
 
 
 app = FastAPI(title=settings.app_title, description=settings.app_description)
+admin = Admin(app, engine)
 
+
+class UserAdmin(ModelView, model=User):
+    column_list = [User.id, User.name]
+    column_searchable_list = [User.name, User.surname]
+
+
+admin.add_view(UserAdmin)
 
 # uvicorn app.main:app --reload
