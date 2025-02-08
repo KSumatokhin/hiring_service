31ae2c2365689fd69aa9647861f5f62628f1bf09
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2025-02-08 12:52:14 +0200

    начало проекта

---
+++ app/core/config.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from pydantic import EmailStr
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_title: str = 'Сервис найма'
+    app_description: str = 'API часть для взаимодействия с Telegram-ботом'
+    psg: bool
+    database_psg: str
+    database_sql: str
+    secret: str = 'secret'
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
