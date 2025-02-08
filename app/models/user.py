3618125f36967a7e3c78bf3144d4cd5b87c225d8
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2025-02-08 15:03:54 +0200

    добавил alembic

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, BigInteger, DateTime, Enum
+import enum
+
+from app.core.db import Base
+
+
+class Role(str, enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
+
+
+class User(Base):
+    name = Column(String(20), nullable=False)
+    surname = Column(String(50), nullable=True)
+    tg_id = Column(BigInteger, unique=True, nullable=False)
+    tg_username = Column(String(50), unique=True, nullable=False)
+    birthday = Column(DateTime, nullable=False)
+    role = Column(Enum(Role), default=Role.USER, nullable=False)
+    password = Column(String(20))
+    email = Column(String(50), nullable=False)  # Валидация email?
+    phone = Column(String(50), nullable=False)  # Валидация phone?
